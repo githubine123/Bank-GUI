0772ce093bc79d74308cda982481f76966cc9586
githubine123 <104296018+githubine123@users.noreply.github.com>
2024-06-13 16:45:43 +0200

    Update pyinstaller.cmd

--- pyinstaller.cmd
+++ pyinstaller.cmd
@@ -1,4 +1,4 @@
-// Run this file to install the .exe
-// Run all of the code in Command Prompt
+# Run this file to install the .exe
+# Run all of the code in Command Prompt
 pip install pyinstaller
 pyinstaller --onefile main.py
